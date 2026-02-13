c0b4a29a35124a7fc6b74765c4158428ac5bc423
Ivan Pykhtin <ipykhtin@lab2r1s1.42abudhabi.ae>
2026-02-13 22:25:38 +0400

    changes

--- python_module_04/ex2/ft_stream_management.py
+++ python_module_04/ex2/ft_stream_management.py
@@ -2,4 +2,17 @@ import sys
 
 def ft_stream_management():
     print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
-    
+    try:
+        ID = input("Input Stream active. Enter archivist ID: ")
+        report = input("Input Stream active. Enter status report:")
+
+        sys.stdout.write(f"\n[STANDARD] Archive status from {ID}: {report}\n")
+        sys.stderr.write("[ALERT] System diagnostic: Communication channels verified\n")
+        sys.stdout.write("[STANDARD] Data transmission complete\n")
+        sys.stdout.write("\nThree-channel communication test successful.")
+    except Exception as e:
+        sys.stderr.write(e)
+
+
+if __name__ == "__main__":
+    ft_stream_management()
